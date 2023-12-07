fd1d0fdec5182e9d85686ea1acb830e67f77721f
István Nagy <114640475+NaGyRyT@users.noreply.github.com>
2023-12-07 10:17:05 +0100

    tooltip

--- client/dcd/src/Components/Dashboard/Log/Viewlog/Viewlog.jsx
+++ client/dcd/src/Components/Dashboard/Log/Viewlog/Viewlog.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Button, Modal, ListGroup } from 'react-bootstrap';
+import { Button, Modal, ListGroup, Tooltip, OverlayTrigger  } from 'react-bootstrap';
 
 export default function Viewlog( {logEntry}) {
     const [showViewLogForm, setShowViewLogForm] = useState(false);
@@ -7,43 +7,49 @@ export default function Viewlog( {logEntry}) {
     const handleShowViewLogForm = async() => {
         setShowViewLogForm(true);
     }
+
+    const renderTooltip = (tooltip) => (<Tooltip id="button-tooltip">{tooltip}</Tooltip>);
     
-  return (
+return (
     <>
-    <Button 
-        size="sm"
-        className="m-1"
-        variant="success"
-        onClick={handleShowViewLogForm}>
-        👁
-    </Button>
-    <Modal
-        show={showViewLogForm}
-        onHide={handleCloseViewLogForm}
-        backdrop='static'
-        dialogClassName='modal-80w'>
-        <Modal.Header closeButton>
-            <Modal.Title>Naplóbejegyzés részletek</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <ListGroup>
-                <ListGroup.Item>Napló sorszám: {logEntry.id}</ListGroup.Item>
-                <ListGroup.Item>Felhasználó neve: {logEntry.user_name}</ListGroup.Item>
-                <ListGroup.Item>Ügyfél neve: {logEntry.client_name}</ListGroup.Item>
-                <ListGroup.Item>Ügyféltalálkozás dátuma, ideje: {logEntry.date_time}</ListGroup.Item>
-                <ListGroup.Item>Ügyféltalálkozás időtartama: {logEntry.duration}</ListGroup.Item>
-            </ListGroup>
-            <h5 className='mt-3'>Ügyféltalálkozás leírása</h5>
-            <p>{logEntry.description}</p>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button onClick={handleCloseViewLogForm}>
-                Bezár
+        <OverlayTrigger
+            placement="top"
+            delay={{ show: 50, hide: 100 }}
+            overlay={renderTooltip('Részletek')}>
+            <Button 
+                size="sm"
+                className="m-1"
+                variant="success"
+                onClick={handleShowViewLogForm}>
+                👁
             </Button>
+        </OverlayTrigger>
+        <Modal
+            show={showViewLogForm}
+            onHide={handleCloseViewLogForm}
+            backdrop='static'
+            dialogClassName='modal-80w'>
+            <Modal.Header closeButton>
+                <Modal.Title>Naplóbejegyzés részletek</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <ListGroup>
+                    <ListGroup.Item>Napló sorszám: {logEntry.id}</ListGroup.Item>
+                    <ListGroup.Item>Felhasználó neve: {logEntry.user_name}</ListGroup.Item>
+                    <ListGroup.Item>Ügyfél neve: {logEntry.client_name}</ListGroup.Item>
+                    <ListGroup.Item>Ügyféltalálkozás dátuma, ideje: {logEntry.date_time}</ListGroup.Item>
+                    <ListGroup.Item>Ügyféltalálkozás időtartama: {logEntry.duration}</ListGroup.Item>
+                </ListGroup>
+                <h5 className='mt-3'>Ügyféltalálkozás leírása</h5>
+                <p>{logEntry.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={handleCloseViewLogForm}>
+                    Bezár
+                </Button>
+            </Modal.Footer>
 
-        </Modal.Footer>
-
-    </Modal>
+        </Modal>
     </>
   )
 }
