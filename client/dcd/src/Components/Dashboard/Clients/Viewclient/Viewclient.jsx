3ed35e73ef396155f46986a4d826d35e1693c05c
István Nagy <114640475+NaGyRyT@users.noreply.github.com>
2023-12-12 12:40:19 +0100

    new log in viewclient form

--- client/dcd/src/Components/Dashboard/Clients/Viewclient/Viewclient.jsx
+++ client/dcd/src/Components/Dashboard/Clients/Viewclient/Viewclient.jsx
@@ -1,9 +1,13 @@
-import React, { useState  } from 'react'
+import React, { useState  } from 'react';
 import axios from 'axios';
 import { OverlayTrigger, Tooltip, Button, Modal, Row, Col, Table } from 'react-bootstrap';
 import Viewlog from '../../Log/Viewlog/Viewlog'
+import Newlog from '../../Log/Newlog/Newlog';
+import Editlog from '../../Log/Editlog/Editlog';
+import Deletelog from '../../Log/Deletelog/Deletelog';
 
-export default function Viewclient( { listItem } ) {
+
+export default function Viewclient( { listItem, loggedInUserId } ) {
     const [showViewClientForm, setShowViewClientForm] = useState(false);
     const [logEntries, setLogEntries] = useState([]); 
     const handleCloseViewClientForm = () => setShowViewClientForm(false);  
@@ -11,7 +15,6 @@ export default function Viewclient( { listItem } ) {
         await getLog();
         setShowViewClientForm(true);
     }
-    
 
     async function getLog() {
         await axios.get(`http://localhost:8080/getlog/${listItem.id}`)
@@ -20,9 +23,9 @@ export default function Viewclient( { listItem } ) {
           })
       };
     
-    const renderTooltip = (props) => (
+    const renderTooltip = (tooltip) => (
         <Tooltip id="View-button-tooltip" >
-            {props}
+            {tooltip}
         </Tooltip>
         );
     
@@ -31,7 +34,7 @@ export default function Viewclient( { listItem } ) {
             <OverlayTrigger
                 placement="top"
                 delay={{ show: 50, hide: 100 }}
-                overlay={renderTooltip('Megnéz')}
+                overlay={renderTooltip('Részletek/Új napló')}
             >
                 <Button 
                     size="sm"
@@ -41,42 +44,45 @@ export default function Viewclient( { listItem } ) {
                     👁
                 </Button>
             </OverlayTrigger>
-            <Modal show={showViewClientForm} onHide={handleCloseViewClientForm} backdrop='static'>
+            <Modal 
+                className='' 
+                show={showViewClientForm}
+                onHide={handleCloseViewClientForm}
+                dialogClassName='modal-60w'
+                backdrop='static'>
                 <Modal.Header closeButton>
                         <Modal.Title>Ügyfél részletek</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Row className='my-2'>
-                        <Col xs={12} sm={7}>
-                            Név: {listItem.name}
-                        </Col>
-                        <Col xs={12} sm={5}>
-                            Azonosító: {listItem.client_id}
-                        </Col>
+                    <div className='border'>
+                    <Row className='my-2 border-bottom'>
+                        <Col xs={12} sm={7}>Név: {listItem.name}</Col>
+                        <Col xs={12} sm={5}>Azonosító: {listItem.client_id}</Col>
                     </Row>
-                    <Row className='my-2'>
+                    <Row className='my-2 border-bottom'>
                         <Col xs={12} sm={7}>Születési dátum: {listItem.birth_date}</Col>
                         <Col xs={12} sm={2}>Kor: {listItem.age}</Col>
                         <Col xs={12} sm={3}>Nem: {listItem.gender}</Col>                        
                     </Row>
-                    <Row className='my-2'>
+                    <Row className='my-2 border-bottom'>
                         <Col xs={12} sm={7}>E-mail: {listItem.email}</Col>
                         <Col xs={12} sm={5}>Mobil: {listItem.phone}</Col>
                     </Row>
-                    <Row className='my-2'>
+                    <Row className='my-2 border-bottom'>
                         <Col xs={12} sm={7}>Ir. szám: {listItem.zip}</Col>
                         <Col xs={12} sm={5}>Város: {listItem.city}</Col>  
                     </Row>
-                    <Row className='my-2'>
+                    <Row className='my-2 border-bottom'>
                         <Col xs={12} sm={12}>Utca: {listItem.street}</Col>
                     </Row>
-                    <Row className='my-2'>    
+                    <Row className='my-2 border-bottom'>
                         <Col xs={12} sm={4}>Házszám: {listItem.house_number}</Col>
                         <Col xs={12} sm={4}>Emelet: {listItem.floor}</Col>
                         <Col xs={12} sm={4}>Ajtó: {listItem.door}</Col>
                     </Row>
+                    </div>
                     {logEntries.length > 0 ?
-                          <Table striped bordered hover size="sm" >
+                        <Table className='mt-4' striped bordered hover size="sm" >
                             <thead>
                                 <tr><th colSpan={12}>Naplóbejegyzések</th></tr>
                                 <tr>
@@ -95,22 +101,37 @@ export default function Viewclient( { listItem } ) {
                                         <td>{item.user_name}</td>
                                         <td>{item.date_time}</td>
                                         <td>{item.duration}</td>
-                                        <td className='d-none d-sm-table-cell'>{item.description.slice(0,10)}</td>
-                                        <td>
+                                        <td className='d-none d-sm-table-cell'>{item.description.length > 60 ? 
+                                                                                item.description.slice(0,60) + '...' :
+                                                                                item.description}
+                                        </td>
+                                        <td className='width-150'>
                                             <Viewlog logEntry={item}></Viewlog>
+                                            {item.user_id === loggedInUserId ?
+                                            <>
+                                             <Editlog
+                                                logEntry = {item}
+                                                loadLogEntries = {getLog}/>
+                                            <Deletelog
+                                                listItem = {item}
+                                                loadLogEntries = {getLog}/></> : ''
+                                            }
                                         </td>
                                     </tr>)}
                             </tbody>
-                            </Table>
-                        : 
+                        </Table> : 
                         ''
                     }
                 </Modal.Body>
                 <Modal.Footer>
+                    <Newlog
+                        selectedClient={listItem}
+                        loggedInUserId={loggedInUserId}
+                        fromClientList={false}
+                        getLog={getLog}/>
                     <Button onClick={handleCloseViewClientForm}>
                         Bezár
                     </Button>
-
                 </Modal.Footer>
             </Modal>
         </>
